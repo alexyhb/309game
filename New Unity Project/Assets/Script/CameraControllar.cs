93160d2191cc26b6f89ba4844494faae085a786b
yhfei <yhfei@zhengyunkeji.com>
2018-12-10 18:57:30 +0800

    add some perfab and a scene

---
+++ New Unity Project/Assets/Script/CameraControllar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllar : MonoBehaviour {
+
+
+	public GameObject player;
+	public float rotationDamping;
+	public float distance;
+	public float height;
+	// Use this for initialization
+	void Start()
+	{
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	void LateUpdate()
+	{
+		float wantedRotationAngle = player.transform.eulerAngles.y;
+		float currentRotationAngle = transform.eulerAngles.y;
+		// Damp the rotation around the Y-axis
+		currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle,
+			rotationDamping * Time.deltaTime);
+		// Convert the angle into a quaternion
+		Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+		// Set the (x,z) camera position based on where the player is
+		transform.position = player.transform.position;
+		transform.position -= currentRotation * Vector3.forward * distance;
+		// Set the height (y) of the camera
+		Vector3 newPosition = new Vector3(transform.position.x, height, transform.position.z);
+		transform.position = newPosition;
+		// Look at the player
+		transform.LookAt(player.transform);
+	}
+}
---
+++ New Unity Project/Assets/Script/GamerControllar.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Boundary
+{
+	public float xMin,xMax,zMin,zMax;
+}
+public class GamerControllar : MonoBehaviour {
+	public float speed;
+	public float turnSpeed;
+	public GameObject shot;
+	public Transform shotSpawn;
+	// public float tilt;
+	public float fireRate;
+	private float nextFire;
+	public GameObject playerExplosion;
+	public GameObject partExplosion;
+	public Boundary boundary;
+
+	public int HP;
+//	public GameController controller;
+	// Use this for initialization
+	void Start () {
+//		HPStrip.value = HPStrip.maxValue = HP;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("space") && Time.time>nextFire)
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		// Turning
+		if (moveHorizontal != 0)
+		{
+			transform.Rotate(new Vector3(0.0f, moveHorizontal * turnSpeed, 0.0f));
+		}
+		// Forward or backwards
+		if (moveVertical != 0)
+		{
+			Vector3 fwd = transform.forward;
+			GetComponent<Rigidbody>().velocity = fwd * speed * moveVertical;
+		}
+		GetComponent<Rigidbody>().position = new Vector3(
+			Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax),
+			2.0f,
+			Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax));
+
+	}
+	// void OnTriggerEnter(Collider other)
+	// 	{
+
+	// 		if (other.tag == "bolt2")
+	// 		{
+	// 			HP -= 1;
+	// 			HPStrip.value = HP;
+	// 			if(HPStrip.value !=0)
+	// 			{
+	// 				Instantiate(partExplosion, transform.position, transform.rotation);
+	// 			}
+	// 			if (HPStrip.value == 0)
+	// 			{
+
+	// 				Destroy(other.gameObject);
+	// 				Destroy(gameObject);
+	// 				Instantiate(playerExplosion, transform.position, transform.rotation);
+	// 				controller.EndGame();
+	// 			}      
+	// 		}
+	// 	}
+}
